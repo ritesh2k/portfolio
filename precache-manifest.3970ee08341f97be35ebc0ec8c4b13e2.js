6f5080d005f7529a8328460b9f1402dd856421aa
= ritesh2k <=ritesh2k@yahoo.com>
2020-07-25 13:48:13 +0530

    Updates

---
+++ precache-manifest.3970ee08341f97be35ebc0ec8c4b13e2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06d20b18c2255890bc078360d0479d02",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8db78511828b87fdeda3",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8ab3c767218a9ae392bb",
+    "url": "/portfolio/static/css/main.c0d7595b.chunk.css"
+  },
+  {
+    "revision": "8db78511828b87fdeda3",
+    "url": "/portfolio/static/js/2.dce9a316.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.dce9a316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ab3c767218a9ae392bb",
+    "url": "/portfolio/static/js/main.38bd238a.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "f070931452acf1d619ca8c11e4b52420",
+    "url": "/portfolio/static/media/78592.f0709314.jpg"
+  },
+  {
+    "revision": "8462e5e50b6d6ee23d446fd2429d6263",
+    "url": "/portfolio/static/media/EncodeSansSemiExpanded-Medium.8462e5e5.ttf"
+  }
+]);
