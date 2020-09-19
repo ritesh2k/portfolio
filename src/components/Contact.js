9f26c04fa35d7bb1ab88f2b9e77e7e2835e91e9f
ritesh2k <ritesh2k@yahoo.com>
2020-09-19 19:42:56 +0530

    remove un-necessary packages

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,26 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const payload = {
+    from_name: email,
+    message: message,
+  };
+
+  // Email JS
+  const sendMessage = () =>
+    window.emailjs.send("gmail", "template_vbcsgpz", payload);
+
   return (
     <div className="contact-div">
       <h1>Get in Touch</h1>
 
-      <form>
-        <div className = "wrapper">
+      <form onSubmit={(event) => event.preventDefault()}>
+        <div className="wrapper">
           <div>
             <label htmlFor="name">Name</label>
-            <input className="" type="text" name="name" id="name" />
+            <input
+              className=""
+              type="text"
+              name="name"
+              id="name"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+            />
           </div>
 
           <div>
             <label htmlFor="email">Email</label>
-            <input className="" type="email" name="email" id="email" />
+            <input
+              className=""
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
           </div>
         </div>
 
         <label htmlFor="message">Message</label>
-        <textarea name="message" id="message" rows="5" />
-        <button>Send</button>
+        <textarea
+          name="message"
+          id="message"
+          rows="5"
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+        />
+        <button onClick={sendMessage}>Send</button>
       </form>
     </div>
   );
