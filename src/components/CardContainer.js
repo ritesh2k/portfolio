b0fc47b8279ff1e0d9ded57cec5135a249a3702d
ritesh <19952553+ritesh2k@users.noreply.github.com>
2023-09-17 19:05:41 +0530

    update skills

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -6,9 +6,9 @@ import NetworkIcon from "./svgs/NetworkIcon";
 import ToolsIcon from "./svgs/ToolsIcon";
 
 const CardContainer = () => {
-  const frameWork = "React.js, React-Router, Redux,";
-  const design = "HTML, CSS, Material UI, Bootstrap .";
-  const network = "HTTPS, JWT, Cookies, Local Storage.";
+  const frameWork = "React.js, React-Router, Redux, Redux-toolkit";
+  const design = "HTML, CSS, Material UI, Tailwind, Bootstrap";
+  const network = "RTK/query, Axios, Fetch API";
   const css = "Git, Postman, React-Proto, VS Code";
 
   return (
